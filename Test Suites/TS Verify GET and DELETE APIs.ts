ab13ee7e6c9ed6f66168ae8c0f4323617097e1dd
Hafidz Firmansyah Ghufara <hafidzfg@gmail.com>
2022-07-12 19:41:33 +0700

    commit test suites

---
+++ Test Suites/TS Verify GET and DELETE APIs.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Verify GET and DELETE APIs</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>90bfbf58-1dd1-42ec-9a34-e2b818833fca</testSuiteGuid>
+   <testCaseLink>
+      <guid>c25ea451-bd53-45e2-aa5a-10a5863cefac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GT 001_Verify All Get Datas API</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b518e6d-8acf-4c61-938c-9cd1ebc7dbe2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DL 001_Verify All Delete Datas API</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
